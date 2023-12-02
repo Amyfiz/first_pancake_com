5151d96fe3b90ad18ed22ea6498973222cf51e69
Amyfiz <solonukhina@sfedu.ru>
2023-12-02 23:57:06 +0300

    feat: Add add_receipt method

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -4,4 +4,11 @@ class Receipt < ApplicationRecord
   belongs_to :user
   has_many :favourites
   has_and_belongs_to_many :categories
+
+  def self.add_recipe(title, description, photo)
+    create(title: title, description: description)
+    puts "Recipe successfully added!"
+  rescue => e
+    puts e.message
+  end
 end
