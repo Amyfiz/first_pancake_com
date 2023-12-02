3696d6099f340ac6fb26a20db81a6664763bb306
Amyfiz <solonukhina@sfedu.ru>
2023-12-03 00:19:12 +0300

    rename add_receipt

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -5,7 +5,7 @@ class Receipt < ApplicationRecord
   has_many :favourites
   has_and_belongs_to_many :categories
 
-  def self.add_recipe(title, description, photo)
+  def self.add_receipt(title, description, photo)
     create(title: title, description: description)
     puts "Recipe successfully added!"
   rescue => e
