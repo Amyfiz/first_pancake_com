005856572845054a5e403908886f0118ccdb9567
Sergey Gnatenko <gnaten.ko2004@gmail.com>
2023-12-18 00:19:00 +0300

    added column profile_image to Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_02_224524) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_17_211322) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_02_224524) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest", null: false
+    t.string "profile_image"
   end
 
   add_foreign_key "subscriptions", "users", column: "follow_id"
